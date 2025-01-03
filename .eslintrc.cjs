8991784ba8f21c49116fd6d8a400e9371c62325e
unknown <hspcadmin@DESKTOP-95JHHMM.hs.handsome.com.cn>
2025-01-03 14:09:53 +0800

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,14 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2021: true, node: true },
-	extends: ["eslint:recommended", "plugin:prettier/recommended"],
+	extends: ["eslint:recommended", 'prettier', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
 	ignorePatterns: ["dist", ".eslintrc.cjs", ".prettierrc.cjs"],
 	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh", "react", "@typescript-eslint", "prettier"],
+	plugins: ["react", "@typescript-eslint", 'prettier'],
 	rules: {
-		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
-    "no-unused-vars": "warn",
-    // 强制所有控制语句使用一致的括号风格
-    "curly": "error"
+    // "arrow-body-style": "off",
+    // "prefer-arrow-callback": "off",
+    // "no-unused-vars": "warn",
 	},
   "parserOptions": {
     "ecmaFeatures": {
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,3 @@
-// 配置几个常用的就可以
 module.exports = {
   // 一行最多多少个字符
   "printWidth": 150,
@@ -33,10 +32,6 @@ module.exports = {
   "insertPragma": false,
   // 使用默认的折行标准 always\never\preserve
   "proseWrap": 'preserve',
-  // 指定HTML文件的全局空格敏感度 css\strict\ignore
-  "htmlWhitespaceSensitivity": 'css',
-  // Vue文件脚本和样式标签缩进
-  "vueIndentScriptAndStyle": false,
   // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
   "endOfLine": 'auto'
 };
